ab066cc96b65b2d060731dc7c6365640ba93fec3
James Gill <jjtech@jjtech.dev>
2025-11-02 00:56:21 -0400

    fix off-by-one error in SA negotiation

--- swu_emulator.py
+++ swu_emulator.py
@@ -1881,8 +1881,8 @@ class swu():
         return return_list
 
     def set_sa_negotiated(self,num):
-        sa_negotiated = self.sa_list[num-1]
-        self.sa_list_negotiated = [self.sa_list[num-1]]
+        sa_negotiated = self.sa_list[num]
+        self.sa_list_negotiated = [self.sa_list[num]]
         
         #default values
         self.negotiated_integrity_algorithm = NONE
@@ -1909,9 +1909,9 @@ class swu():
         
 
     def set_sa_negotiated_child(self,num):
-        sa_negotiated = self.sa_list_child[num-1]
-        self.spi_init_child = self.sa_spi_list[num-1]
-        self.sa_list_negotiated_child = [self.sa_list_child[num-1]]
+        sa_negotiated = self.sa_list_child[num]
+        self.spi_init_child = self.sa_spi_list[num]
+        self.sa_list_negotiated_child = [self.sa_list_child[num]]
         
         #default values
         self.negotiated_integrity_algorithm_child = NONE
